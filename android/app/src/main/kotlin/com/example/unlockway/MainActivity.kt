0da6ceeffaad6351b6e774fd1de5af256a679140
DanielVieiraSS <danielvieira2828@gmail.com>
2023-06-10 10:21:04 -0300

    Atualização do Design

---
+++ android/app/src/main/kotlin/com/example/unlockway/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.unlockway
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// Primary Color
+var primary = const Color.fromARGB(255, 215, 255, 130);
+var primarylight = const Color.fromARGB(255, 209, 248, 127);
+var primarydark = const Color.fromARGB(255, 16, 148, 63);
+
+// Dark Bgcolor
+
+var darkBg = const Color.fromARGB(255, 39, 47, 63);
+var darkBglight = const Color.fromARGB(255, 54, 65, 86);
+var darkBgdark = const Color.fromARGB(255, 36, 46, 57);
+
+// Light Bgcolor
+
+var lightBg = const Color.fromARGB(255, 243, 243, 243);
+var lightBglight = const Color.fromARGB(255, 255, 255, 255);
+var lightBgdark = const Color.fromARGB(255, 212, 212, 212);
+
+// Extras
+
+var darkExtra = const Color.fromARGB(255, 33, 33, 33);
+var darkExtra2 = const Color.fromARGB(255, 89, 89, 89);
+var danger = const Color.fromARGB(255, 195, 52, 52);
+var success = const Color.fromARGB(255, 82, 204, 131);
