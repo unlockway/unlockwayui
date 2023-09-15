6681a5c61948d63bfce5c10b47032d70237f600d
DanielVieiraSS <danielvieira2828@gmail.com>
2023-09-15 11:05:40 -0300

    Popup refactored

--- lib/screens/login/components/login_popup.dart
+++ lib/screens/login/components/login_popup.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:unlockway/components/buttons.dart';
 import 'package:unlockway/components/text_field.dart';
 import 'package:unlockway/components/navigation.dart';
-import 'package:unlockway/constants.dart';
 import 'package:unlockway/screens/home/home.dart';
 
 class LoginPopup extends StatelessWidget {
