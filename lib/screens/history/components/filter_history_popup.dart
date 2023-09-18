b5f3113bb114b7d82d2b04b0dcf213a83affedde
DanielVieiraSS <danielvieira2828@gmail.com>
2023-09-18 20:55:48 -0300

    popups finished

--- lib/screens/history/components/filter_history_popup.dart
+++ lib/screens/history/components/filter_history_popup.dart
@@ -2,23 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:unlockway/components/text_field.dart';
 import 'package:unlockway/constants.dart';
 
-class FilterRoutinePopup extends StatelessWidget {
-  const FilterRoutinePopup({super.key});
+class FilterHistoryPopup extends StatelessWidget {
+  const FilterHistoryPopup({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-      ),
       margin: const EdgeInsets.all(20),
       child: Form(
         child: Column(
           children: [
-            const GenericTextField(
-                title: "Categoria", placeholder: "", width: double.infinity),
-            const SizedBox(height: 20),
             const GenericTextField(
                 title: "Data de criação",
                 placeholder: "dd/mm/yyyy",
--- lib/screens/history/history.dart
+++ lib/screens/history/history.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:unlockway/components/bottom_navigator.dart';
+import 'package:unlockway/components/popups.dart';
 import 'package:unlockway/constants.dart';
+import 'package:unlockway/screens/history/components/filter_history_popup.dart';
 
 class History extends StatelessWidget {
   const History({super.key});
@@ -9,6 +11,97 @@ class History extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(darkBgdark),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(150),
+        child: SafeArea(
+          child: Container(
+            margin:
+                const EdgeInsets.only(bottom: 10, left: 10, right: 10, top: 10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 5,
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "HISTÓRICO",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Inter",
+                        color: Colors.white),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: TextField(
+                        cursorColor: const Color.fromARGB(255, 155, 155, 155),
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          enabled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor: Color(darkBglight),
+                          contentPadding: const EdgeInsets.all(10.0),
+                          hintText: "Buscar",
+                          suffixIcon: Icon(
+                            Icons.search,
+                            color: Color(lightBglight),
+                          ),
+                          border: InputBorder.none,
+                          hintStyle: TextStyle(
+                            color: Color(lightBg),
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Material(
+                      type: MaterialType.transparency,
+                      child: Ink(
+                        decoration: BoxDecoration(
+                          color: Color(darkBglight),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: InkWell(
+                          //borderRadius: BorderRadius.circular(100.0),
+                          onTap: () => modalBuilderBottomAnimation(
+                            context,
+                            const FilterHistoryPopup(),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Icon(
+                              Icons.filter_list,
+                              size: 25.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       bottomNavigationBar: const UBottomNavigator("History"),
     );
   }
