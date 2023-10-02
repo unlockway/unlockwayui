fd967bbba5f5df4ccf89d26f92fc31668bc25ee5
DanielVieiraSS <danielvieira2828@gmail.com>
2023-10-02 16:31:22 -0300

    .

--- lib/screens/home/components/home_graph.dart
+++ lib/screens/home/components/home_graph.dart
@@ -5,6 +5,12 @@ class HomeGraph extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Column(
+        children: [
+          Text(""),
+        ],
+      ),
+    );
   }
 }
