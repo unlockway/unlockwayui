401809f6d4757c2c3bf6d09e4ae483b728196d08
Victor Silva <victor.silvapc@hotmail.com>
2023-11-29 04:12:20 -0300

    chore: removing unecessary imports

--- lib/screens/meals/meals.dart
+++ lib/screens/meals/meals.dart
@@ -4,14 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:unlockway/components/bottom_navigator.dart';
 import 'package:unlockway/components/navigation.dart';
-import 'package:unlockway/components/popups.dart';
 import 'package:unlockway/constants.dart';
 import 'package:unlockway/handlers/meals.handlers.dart';
 import 'package:unlockway/models/ingredients.dart';
 import 'package:unlockway/models/meals.dart';
 import 'package:unlockway/screens/meals/components/meal_card.dart';
 import 'package:unlockway/screens/meals/components/meal_form.dart';
-import 'package:unlockway/screens/meals/components/new_meal_popup.dart';
 
 class Meals extends StatefulWidget {
   const Meals({super.key});
